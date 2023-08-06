6d2c40cc009203b47fe81f32915dc6194b9596fe
Valentina Rogojina <rogojinavaliusha@gmail.com>
2023-08-06 17:04:45 +0300

    Exemlu1

---
+++ Exemplu1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+// Ввод трехзначного числа с клавиатуры
+            Console.Write(Console.Write"Введите трехзначное число: ");
+            
+           int number = int.Parse(Console.ReadLine());
+
+// Находим вторую цифру числа
+            
+           int secondDigit = (number / 10) % 10;
+
+           // Вывод результата
+            Console.WriteLine(Console.WriteLine($"Вторая цифра числа: {secondDigit}"));
+
