a48f92806553852ffe3e827fd35ae73cdd47c036
Valentina Rogojina <rogojinavaliusha@gmail.com>
2023-08-06 17:32:56 +0300

    Exemplu2

--- Exemplu1/Program.cs
+++ Exemplu1/Program.cs
@@ -1,14 +1,21 @@
-﻿
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
 
-// Ввод трехзначного числа с клавиатуры
-            Console.Write(Console.Write"Введите трехзначное число: ");
+
+  static void Main(string[] args)
+  {
+
+// Ввод трехзначного числа с клавиатуры.
+            Console.Write(Console.Write"Введите  число: ");
             
            int number = int.Parse(Console.ReadLine());
 
-// Находим вторую цифру числа
+// Находим вторую цифру числа.
             
            int secondDigit = (number / 10) % 10;
 
            // Вывод результата
-            Console.WriteLine(Console.WriteLine($"Вторая цифра числа: {secondDigit}"));
-
+            Console.WriteLine(Console.WriteLine($"Rezult: {secondDigit}"));
+  }
